6dc27486c9a2444a74b323c1d2b4283b202e9abe
JamesFrowen-PC\JamesFrowen <jamesfrowendev@gmail.com>
2018-05-08 16:57:52 +0100

    refreshing database after creating asset

--- source/Editor/MergeMeshWindow.cs
+++ source/Editor/MergeMeshWindow.cs
@@ -69,6 +69,7 @@ namespace MergeMeshUnity
             if (GUILayout.Button("export"))
             {
                 ObjExporterScript.DoExport(true);
+                AssetDatabase.Refresh();
             }
         }
 
@@ -116,6 +117,7 @@ namespace MergeMeshUnity
                 string fileName = EditorUtility.SaveFilePanelInProject("Export mesh file", "mesh_" + name, "asset", "");
                 AssetDatabase.CreateAsset(filter.sharedMesh, fileName);
                 AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
 
                 Selection.activeGameObject = go;
             }
